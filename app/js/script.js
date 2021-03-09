64e619a867345895bc2f0b14d3b5fa5cf784644a
Ken Quach <kenquach@Kens-Mac-mini.local>
2021-03-09 16:56:58 +1000

    Added first-time remaining file

---
+++ app/js/script.js
@@ -0,0 +1,10 @@
+console.log("This is a website")
+
+var app = new Vue({
+    el: '#app',
+    data:{
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        image: '/img/vmSocks-green-onWhite.jpg'
+    }
+})
