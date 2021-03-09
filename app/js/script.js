7b469ebe5dbc765c05c7720b002159e4b9c3ed8c
Ken Quach <kenquach@Kens-Mac-mini.local>
2021-03-09 18:29:28 +1000

    up to lesson 4

--- app/js/script.js
+++ app/js/script.js
@@ -5,6 +5,23 @@ var app = new Vue({
     data:{
         product: 'Socks',
         description: 'A pair of warm, fuzzy socks',
-        image: '/img/vmSocks-green-onWhite.jpg'
+        image: '/img/vmSocks-green-onWhite.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inventory: 5,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+              variantId: 2234,
+              variantColor: 'green'    
+            },
+            {
+              variantId: 2235,
+              variantColor: 'blue'
+            }
+          ],
+          sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
+
+
